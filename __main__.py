a08b07eb4f0a4b9a860e0173f17196958d18de17
bkroll <10960255+bkroll@users.noreply.github.com>
2018-09-04 17:34:24 +0200

    common starting point for both workflows

---
+++ __main__.py
@@ -0,0 +1,17 @@
+print('\n')
+print('pdfcut v3.1 - record and apply granular page numbers')
+print('----------------------------------------------------\n')
+
+print('Bitte Workflow auswählen:')
+print('  1 - Erfassung von Seitenzahlen')
+print('  2 - Granulare PDFs anhand erfasster Seitenzahlen erstellen\n')
+
+stepChoice = input("Workflow: ")
+print('\n\n\n')
+
+if str(stepChoice) == '1':
+    __import__('step1')
+elif str(stepChoice) == '2':
+    __import__('step2')
+else:
+    print('Ungültige Auswahl.')
