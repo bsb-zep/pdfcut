d4e4a726ebb1db24e89b46a0160d41ecf1a62e14
bkroll <10960255+bkroll@users.noreply.github.com>
2018-09-04 10:48:52 +0200

    manually merge offset handler and manual directory selector

--- 02 - csv anwenden.py
+++ 02 - csv anwenden.py
@@ -3,19 +3,25 @@ import io
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from tkinter import filedialog
 
-def applycsv(firstpage, lastpage, sourcepdf, destinationpdfprefix):
+def applycsv(firstpage, lastpage, sourcepdf, destinationpdfprefix, outputTargetDir):
     output = PdfFileWriter()
     firstpage -= 1
     input = PdfFileReader(open(sourcepdf, "rb"))
     for i in range(firstpage, lastpage):
         output.addPage(input.getPage(i))
-    outputStream = io.FileIO("H:/OJSziel/" + destinationpdfprefix + str(applycounter) + ".pdf", "wb")
-    output.write(outputStream)
-    outputStream.close()
+    outputTargetUri = outputTargetDir + '/' + destinationpdfprefix + str(applycounter) + ".pdf"
+    print('  Ziel: ' + outputTargetUri)
+    try:
+        outputStream = io.FileIO(outputTargetUri, "wb")
+        output.write(outputStream)
+        outputStream.close()
+    except:
+        raise RuntimeError
+    print('\n')
 
 csvlist = list()
 
-print("")
+print('\n')
 newcsvfilepath = filedialog.askopenfilename(title = "Quell-Datei für Seitenangaben auswählen", filetypes = (('CSV-Dateien', '.csv'), ))
 csvfilename = newcsvfilepath
 with open(csvfilename, newline='') as f:
@@ -26,7 +32,11 @@ with open(csvfilename, newline='') as f:
 csvlistlength = len(csvlist)
 print("- Es wurden " + str(csvlistlength) + " Datensätze ausgelesen")
 
-print("")
+print('\n')
+outputTargetDir = filedialog.askdirectory(initialdir="/",title='Bitte Zielverzeichnis für erstellte PDFs wählen')
+print('Speichere PDFs nach: ' +  str(outputTargetDir) )
+
+print('\n')
 csvOffset = input("- Artikel-Offset (Eingabetaste für kein): ")
 if csvOffset == "":
     csvOffset = 0
@@ -34,7 +44,7 @@ if csvOffset == "":
 else:
     print("  Es werden jeweils " + csvOffset + " Seiten vor und nach einem Artikelumbruch mit gespeichert.")
 
-print("")
+print('\n')
 totalOffset = input("- Seitenverschiebung (Eingabetaste für kein): ")
 if totalOffset == "":
     totalOffset = 0
@@ -42,12 +52,12 @@ if totalOffset == "":
 else:
     print("  Die  " + totalOffset + " ersten Seiten der Quell-PDF-Datei werden in der Zählung übersprungen.")
 
-print("")
+print('\n')
 for i in range(csvlistlength):
     if int(csvlist[i][0]) > int(csvlist[i][1]):
         print("- Fehler im Seitenbereich: " + str(csvlist[i]))
 print("- Prüfung der Seitenbereichsdaten beendet.")
-print("")
+print('\n')
 
 singlepagepdf = 0
 for i in range(csvlistlength):
@@ -57,10 +67,10 @@ print("- Prüfangaben für das Inhaltsverzeichnis:")
 print("  Anzahl Elemente: " + str(csvlistlength))
 print("  Einzelseiten-Elemente: " + str(singlepagepdf))
 
-print("")
+print('\n')
 csvdecision = input("- PDF-Dateien generieren? (j/n)")
 
-if csvdecision == "j":
+if csvdecision == str("j"):
     while True:
         try:            
             partfilename = filedialog.askopenfilename(title = "Quell-Datei für PDF auswählen", filetypes = (('PDF-Dateien', '.pdf'), ))
@@ -71,7 +81,7 @@ if csvdecision == "j":
             break
         except FileNotFoundError:
             print("  Quelldatei unter angegebenem Namen nicht gefunden.")
-    print("")
+    print('\n')
     partdestinationname = str(input("  Präfix der Zieldateien ohne Endung: ") + "-")
     csvdecision = input("- Fortfahren? (j/n)")
 if csvdecision == "j":
@@ -79,9 +89,9 @@ if csvdecision == "j":
     for i in range(csvlistlength):
         print("  Verarbeite Seitenabschnitt " + str(1+i) + " von " + str(csvlistlength) + "...")
         applycounter += 1
-        applycsv(int(csvlist[i][0]) - int(csvOffset) + int(totalOffset), int(csvlist[i][1]) + int(csvOffset) + int(totalOffset), completefilename, partdestinationname)
+        applycsv(int(csvlist[i][0]) - int(csvOffset) + int(totalOffset), int(csvlist[i][1]) + int(csvOffset) + int(totalOffset), completefilename, partdestinationname, outputTargetDir)
         print("  Verarbeitung abgeschlossen.")
-    print("")
-    print("PDF-Segmente abgelegt nach H:/OJSziel/ mit dem Präfix " + partdestinationname + ".")
+    print('\n')
+    print("PDF-Segmente abgelegt nach " + outputTargetDir + " mit dem Präfix " + partdestinationname + ".")
 else:
     exit()
